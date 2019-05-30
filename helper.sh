abdf7a728ad289fad5d5c0b49701c434f93ad634
daelvn <daelvn@gmail.com>
2019-05-30 17:01:15 +0200

    Added Humanidad and A la Infancia

---
+++ helper.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+#echo '<p align="center">' | cat - "$1" > temp && mv temp "$1"
+sed -i '1i<p align="center">' "$1"
+echo '</p>' >> "$1"
+sed '1n; $n; s/$/<\/p>/' "$1" > "$1.t" && mv "$1.t" "$1"
