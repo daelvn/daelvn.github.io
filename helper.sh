53b152643967d6a5660050de96918d35b84e4476
daelvn <daelvn@gmail.com>
2019-05-30 17:05:27 +0200

    I have serious problems

--- helper.sh
+++ helper.sh
@@ -2,4 +2,4 @@
 #echo '<p align="center">' | cat - "$1" > temp && mv temp "$1"
 sed -i '1i<p align="center">' "$1"
 echo '</p>' >> "$1"
-sed '1n; $n; s/$/<\/p>/' "$1" > "$1.t" && mv "$1.t" "$1"
+sed '1n; $n; s/$/<br>/' "$1" > "$1.t" && mv "$1.t" "$1"
