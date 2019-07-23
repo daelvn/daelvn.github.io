15591f01e4c40e16924547a21fed99aced7bfc21
Cristian Muñiz <daelvn@gmail.com>
2019-07-23 13:31:02 +0200

    added four new poems

---
+++ images/reifnir/mapping.lua
@@ -0,0 +1,5 @@
+return {
+  ["reifnir-shot1"] = 85779,
+  ["reifnir-shot2"] = 29757,
+  ["reifnir-shot3"] = 104201
+}
---
+++ images/reifnir/mapping.moon
@@ -0,0 +1,5 @@
+{
+  ["reifnir-shot1"]: 85779
+  ["reifnir-shot2"]: 29757
+  ["reifnir-shot3"]: 104201
+}
